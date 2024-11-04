0ec2f2e44e3465c798c41b5d1a18e6fbd02fcc3c
Marc-Antoine Ruel <maruel@gmail.com>
2024-11-04 13:49:45 -0500

    Add basic bfloat16 analysis; unconfirmed for validity
    Unsurprisingly it's orders of magnitude faster than python.

--- cmd/n-bits/main.go
+++ cmd/n-bits/main.go
@@ -14,13 +14,16 @@ import (
 	"github.com/nlpodyssey/safetensors"
 )
 
-func run(ctx context.Context, hf_token, hf_repo string) error {
-	hf, err := huggingface.New(hf_token, "")
+func calcHistogram(t string) {
+}
+
+func run(ctx context.Context, hfToken, hfRepo string) error {
+	hf, err := huggingface.New(hfToken, "")
 	if err != nil {
 		return err
 	}
-	if hf_repo != "" {
-		p, err := hf.EnsureFile(ctx, huggingface.PackedFileRef("hf:"+hf_repo+"/HEAD/model.safetensors"), 0o666)
+	if hfRepo != "" {
+		p, err := hf.EnsureFile(ctx, huggingface.PackedFileRef("hf:"+hfRepo+"/HEAD/model.safetensors"), 0o666)
 		if err != nil {
 			return err
 		}
@@ -34,17 +37,43 @@ func run(ctx context.Context, hf_token, hf_repo string) error {
 		}
 		fmt.Printf("len = %d\n", s.Len())
 		fmt.Printf("names = %+v\n", s.Names())
+		tensors := s.Tensors()
+		for i := range 3 {
+			t := tensors[i].TensorView
+			if t.DType() != safetensors.BF16 {
+				return fmt.Errorf("can't handle dtype %s", t.DType())
+			}
+			data := t.Data()
+			signs := [1 << 1]int{}
+			exponents := [1 << 8]int{}
+			mantissas := [1 << 7]int{}
+			for i := range t.DataLen() / 2 {
+				// Probably a bit faster than proper unpacking via encoding/binary.
+				b1 := data[2*i]
+				b2 := data[2*i+1]
+				sign := (b1 & 0x80) >> 7
+				exponent := ((b1 & 0x7F) << 1) | ((b2 & 0x80) >> 7)
+				mantissa := b2 & 0x7F
+				signs[sign]++
+				exponents[exponent]++
+				mantissas[mantissa]++
+			}
+			fmt.Printf("Tensor %s\n", tensors[i].Name)
+			fmt.Printf("- signs = %+v\n", signs)
+			fmt.Printf("- exponents = %+v\n", exponents)
+			fmt.Printf("- mantissas = %+v\n", mantissas)
+		}
 	}
 	return nil
 }
 
 func mainImpl() error {
-	hf_token := flag.String("hf-token", "", "HuggingFace token")
-	hf_repo := flag.String("hf-repo", "", "HuggingFace repository, e.g. \"meta-llama/Llama-3.2-1B\"")
+	hfToken := flag.String("hf-token", "", "HuggingFace token")
+	hfRepo := flag.String("hf-repo", "", "HuggingFace repository, e.g. \"meta-llama/Llama-3.2-1B\"")
 	flag.Parse()
 
 	ctx := context.Background()
-	return run(ctx, *hf_token, *hf_repo)
+	return run(ctx, *hfToken, *hfRepo)
 }
 
 func main() {
