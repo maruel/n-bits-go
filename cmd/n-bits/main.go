c7ba2bf0197bfba136a77b742b6531a2389ebe42
Marc-Antoine Ruel <maruel@gmail.com>
2024-11-04 14:24:38 -0500

    Improvements

--- cmd/n-bits/main.go
+++ cmd/n-bits/main.go
@@ -6,19 +6,54 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
+	"log/slog"
 	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"time"
 
+	"github.com/lmittmann/tint"
 	"github.com/maruel/sillybot/huggingface"
+	"github.com/mattn/go-colorable"
+	"github.com/mattn/go-isatty"
 	"github.com/nlpodyssey/safetensors"
 )
 
-func calcHistogram(t string) {
+func calcHistogram(t safetensors.TensorView) ([]int, []int, []int) {
+	data := t.Data()
+	signs := [1 << 1]int{}
+	exponents := [1 << 8]int{}
+	mantissas := [1 << 7]int{}
+	for i := range t.DataLen() / 2 {
+		sign, exponent, mantissa := unpackBFloat16(data[2*i:])
+		signs[sign]++
+		exponents[exponent]++
+		mantissas[mantissa]++
+	}
+	return signs[:], exponents[:], mantissas[:]
+}
+
+func unpackBFloat16(data []byte) (int, int, int) {
+	// Probably a bit faster than proper unpacking via encoding/binary.
+	b1 := data[0]
+	b2 := data[1]
+	sign := (b1 & 0x80) >> 7
+	exponent := ((b1 & 0x7F) << 1) | ((b2 & 0x80) >> 7)
+	mantissa := b2 & 0x7F
+	return int(sign), int(exponent), int(mantissa)
 }
 
 func run(ctx context.Context, hfToken, hfRepo string) error {
-	hf, err := huggingface.New(hfToken, "")
+	// TODO: The library kinda sucks for downloading and caching.
+	cache := filepath.Join(".cache", hfRepo)
+	if err := os.MkdirAll(cache, 0o777); err != nil {
+		return err
+	}
+	hf, err := huggingface.New(hfToken, cache)
 	if err != nil {
 		return err
 	}
@@ -35,30 +70,17 @@ func run(ctx context.Context, hfToken, hfRepo string) error {
 		if err != nil {
 			return err
 		}
-		fmt.Printf("len = %d\n", s.Len())
-		fmt.Printf("names = %+v\n", s.Names())
+		//fmt.Printf("len = %d\n", s.Len())
+		//fmt.Printf("names = %+v\n", s.Names())
 		tensors := s.Tensors()
 		for i := range 3 {
 			t := tensors[i].TensorView
-			if t.DType() != safetensors.BF16 {
-				return fmt.Errorf("can't handle dtype %s", t.DType())
+			name := tensors[i].Name
+			if dt := t.DType(); dt != safetensors.BF16 {
+				return fmt.Errorf("%s: can't handle dtype %s", name, dt)
 			}
-			data := t.Data()
-			signs := [1 << 1]int{}
-			exponents := [1 << 8]int{}
-			mantissas := [1 << 7]int{}
-			for i := range t.DataLen() / 2 {
-				// Probably a bit faster than proper unpacking via encoding/binary.
-				b1 := data[2*i]
-				b2 := data[2*i+1]
-				sign := (b1 & 0x80) >> 7
-				exponent := ((b1 & 0x7F) << 1) | ((b2 & 0x80) >> 7)
-				mantissa := b2 & 0x7F
-				signs[sign]++
-				exponents[exponent]++
-				mantissas[mantissa]++
-			}
-			fmt.Printf("Tensor %s\n", tensors[i].Name)
+			fmt.Printf("Tensor %s\n", name)
+			signs, exponents, mantissas := calcHistogram(t)
 			fmt.Printf("- signs = %+v\n", signs)
 			fmt.Printf("- exponents = %+v\n", exponents)
 			fmt.Printf("- mantissas = %+v\n", mantissas)
@@ -68,11 +90,64 @@ func run(ctx context.Context, hfToken, hfRepo string) error {
 }
 
 func mainImpl() error {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	defer stop()
+	programLevel := &slog.LevelVar{}
+	programLevel.Set(slog.LevelError)
+	logger := slog.New(tint.NewHandler(colorable.NewColorable(os.Stderr), &tint.Options{
+		Level:      programLevel,
+		TimeFormat: "15:04:05.000", // Like time.TimeOnly plus milliseconds.
+		NoColor:    !isatty.IsTerminal(os.Stderr.Fd()),
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			switch t := a.Value.Any().(type) {
+			case string:
+				if t == "" {
+					return slog.Attr{}
+				}
+			case bool:
+				if !t {
+					return slog.Attr{}
+				}
+			case uint64:
+				if t == 0 {
+					return slog.Attr{}
+				}
+			case int64:
+				if t == 0 {
+					return slog.Attr{}
+				}
+			case float64:
+				if t == 0 {
+					return slog.Attr{}
+				}
+			case time.Time:
+				if t.IsZero() {
+					return slog.Attr{}
+				}
+			case time.Duration:
+				if t == 0 {
+					return slog.Attr{}
+				}
+			}
+			return a
+		},
+	}))
+	slog.SetDefault(logger)
+	go func() {
+		<-ctx.Done()
+		slog.Info("main", "message", "quitting")
+	}()
+
 	hfToken := flag.String("hf-token", "", "HuggingFace token")
 	hfRepo := flag.String("hf-repo", "", "HuggingFace repository, e.g. \"meta-llama/Llama-3.2-1B\"")
+	verbose := flag.Bool("v", false, "Enable verbose logging")
 	flag.Parse()
-
-	ctx := context.Background()
+	if len(flag.Args()) != 0 {
+		return errors.New("unexpected argument")
+	}
+	if *verbose {
+		programLevel.Set(slog.LevelDebug)
+	}
 	return run(ctx, *hfToken, *hfRepo)
 }
 
--- go.mod
+++ go.mod
@@ -3,14 +3,20 @@ module github.com/maruel/n-bits-go
 go 1.23.1
 
 require (
+	github.com/lmittmann/tint v1.0.5
 	github.com/maruel/sillybot v0.0.0-20240930182418-fa372ed0296c
+	github.com/mattn/go-colorable v0.1.13
+	github.com/mattn/go-isatty v0.0.20
 	github.com/nlpodyssey/safetensors v0.0.0-20230602165149-2a74a2d18984
 )
 
 require (
 	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/schollz/progressbar/v3 v3.14.4 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/term v0.22.0 // indirect
+	github.com/schollz/progressbar/v3 v3.17.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/term v0.25.0 // indirect
 )
+
+// Necessary for determinism.
+//replace github.com/nlpodyssey/safetensors => ../safetensors
